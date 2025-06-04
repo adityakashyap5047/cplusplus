f33b7b64873cb31f53ac74c453a9dba6f5321822
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-06-05 00:43:32 +0530

    power using memoization

--- power.cpp
+++ power.cpp
@@ -4,9 +4,24 @@ using namespace std;
 
 int power(int base, int exp) {
     if (exp == 0) {
-        return 1; // Base case: any number to the power of 0 is 1
+        return 1;
     }
     return base * power(base, exp - 1); // Recursive case
+}
+
+int powerMem(int base, int exp, int pow[]){
+
+    if(exp == 0){
+        return 1; 
+    }
+
+    if(pow[exp]){
+        return pow[exp];
+    }
+
+    pow[exp] = base * powerMem(base, exp - 1, pow);
+
+    return pow[exp];
 }
 
 int main(){
@@ -16,8 +31,15 @@ int main(){
     cout << "Enter exponent: ";
     cin >> exp;
 
+    int* pow = new int[exp + 1]();
+
     int result = power(base, exp);
     cout << "Result: " << result << endl;
+    cout << "Memoized Result: " << powerMem(base, exp, pow);
+
+    cout << endl;
+
+    delete[] pow;
 
     return 0;
 }
